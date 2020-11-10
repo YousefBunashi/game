9db4efa6a001adc201cce32a71ad6ebc4d3fbbcc
YousefBunashi <bunashi88@hotmail.com>
2020-11-10 17:42:23 +0300

    my game need more

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+let randomNumber = Math.round(Math.random() * 10);
+let attemps = 0;
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (e) => setValue(e.target.value);
+  let results = "";
+
+  const onClick = () => {
+    // check users guess against randonNumber
+    attemps++;
+    const userGuess = value;
+    if (userGuess == "") return null;
+    if (attemps == 3) return <p className="alert alert-success">Game over!</p>;
+    if (userGuess == randomNumber)
+      return <p className="alert alert-success">Correct!</p>;
+    if (userGuess > randomNumber)
+      return <p className="alert alert-warning">Too high, guess again</p>;
+    if (userGuess < randomNumber)
+      return <p className="alert alert-warning">Too low, guess again</p>;
+  };
+
+  results = onClick(value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Guess The Number!</h2>
+      <p className="lead">Guess a number between 1 and 10.</p>
+      <input value={value} type="number" onChange={onChange} />
+      <button type="submit" onClick={onClick}>
+        GUESS
+      </button>
+
+      {results}
+    </>
   );
 }
-
 export default App;
---
+++ src/components/GussingBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GussingBar = () => {
+  return <input />;
+};
+
+export default GussingBar;
